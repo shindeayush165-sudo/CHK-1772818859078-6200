c37430571ef65d47141c053bf989a6e70aa27548
shindeayush165-sudo <shindeayush165@gmail.com>
2026-03-10 00:55:38 +0530

    Add IntersectionObserver for video autoplay

---
+++ shorts.js
@@ -0,0 +1,23 @@
+const videos = document.querySelectorAll("video")
+
+const observer = new IntersectionObserver((entries)=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.play()
+
+}else{
+
+entry.target.pause()
+
+}
+
+})
+
+},{threshold:0.7})
+
+videos.forEach(video=>{
+observer.observe(video)
+})
